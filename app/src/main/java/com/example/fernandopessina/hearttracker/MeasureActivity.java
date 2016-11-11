b3d902f79d2bddecdf8e510a8e5e9d02195dd067
GLOBANT\fernando.pessina <fer.pessina1@gmail.com>
2016-11-11 12:11:58 -0300

    WIP

--- app/src/main/java/com/example/fernandopessina/hearttracker/MeasureActivity.java
+++ app/src/main/java/com/example/fernandopessina/hearttracker/MeasureActivity.java
@@ -13,22 +13,35 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
 import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class MeasureActivity extends Activity implements CvCameraViewListener2 {
     private static final String  TAG                 = "OCVSample::Activity";
 
     private CameraBridgeViewBase mOpenCvCameraView;
 
-    private int var1 = 0;
-    private int var2 = 0;
-    private int var3 = 0;
+    Scalar avg;
+
+    private LineGraphSeries<DataPoint> mSeries;
+    private double graphLastXValue = 5d;
+
+    private final int GRAPH_SIZE = 100;
 
     private Mat                  mIntermediateMat;
     private Mat                  rgba;
@@ -63,65 +76,17 @@ public class MeasureActivity extends Activity implements CvCameraViewListener2 {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         setContentView(R.layout.activity_measure);
-        SeekBar bar1 = (SeekBar) findViewById(R.id.seek1);
-        SeekBar bar2 = (SeekBar) findViewById(R.id.seek2);
-        SeekBar bar3 = (SeekBar) findViewById(R.id.seek3);
 
-        bar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                var1 = progress;
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        bar2.setMax(255);
-        bar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                var2 = progress;
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        bar3.setMax(255);
-        bar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                var3 = progress;
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
+        GraphView graph = (GraphView) findViewById(R.id.graphBpm);
+        mSeries = new LineGraphSeries<>(generateData());
+        graph.addSeries(mSeries);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(GRAPH_SIZE);
+        graph.getViewport().setXAxisBoundsManual(true);
 
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.image_manipulations_activity_surface_view);
         mOpenCvCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
+        mOpenCvCameraView.setAlpha(0);
         mOpenCvCameraView.setMaxFrameSize(640,480);
         mOpenCvCameraView.setCvCameraViewListener(this);
     }
@@ -176,18 +141,49 @@ public class MeasureActivity extends Activity implements CvCameraViewListener2 {
 
         Core.split(rgba,planes);
 
-        gray = planes.get(var1);
+        gray = planes.get(2);
 
         Imgproc.equalizeHist(gray,gray);
 
-        Imgproc.threshold(gray, gray, var2, 255, Imgproc.THRESH_BINARY);
+        avg = Core.mean(gray);
 
-        Imgproc.threshold(gray, gray, var2, 255, Imgproc.THRESH_BINARY);
+        ProgressBar avgBar = (ProgressBar) findViewById(R.id.progressBar2);
+
+        int avgVal = (int)avg.val[0]*10;
+        avgBar.setMax(255);
+
+        avgBar.setProgress((int)avg.val[0]);
+
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView bpmTextView = (TextView) findViewById(R.id.textViewBpm);
+                bpmTextView.setText(String.valueOf(avg.val[0]));
+            }
+        });
 
         Imgproc.cvtColor(gray, rgba, Imgproc.COLOR_GRAY2RGBA, 4);
+
+        graphLastXValue += 1d;
+        mSeries.appendData(new DataPoint(graphLastXValue, (int)avg.val[0]), true, 40);
+
 
         for(Mat m : planes)
             m.release();
         return rgba;
+    }
+
+    private DataPoint[] generateData() {
+        int count = GRAPH_SIZE;
+        DataPoint[] values = new DataPoint[count];
+        for (int i=0; i<count; i++) {
+            double x = i;
+            double f = 25;
+            DataPoint v = new DataPoint(x, f);
+            values[i] = v;
+            graphLastXValue = i;
+        }
+        return values;
     }
 }
